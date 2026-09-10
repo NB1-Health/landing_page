be3ce92cfe4db3e752acad50e8a64530fe051a0c
nb1dev <dev@nb1.com>
2026-09-10 16:27:30 +0300

    jounal enabled

--- src/utilities/journalEnabled.ts
+++ src/utilities/journalEnabled.ts
@@ -42,7 +42,7 @@
  *
  * `grep -rn "isJournalEnabled" src` lists every one of them.
  */
-const JOURNAL_ENABLED = false
+const JOURNAL_ENABLED = true
 
 export function isJournalEnabled(): boolean {
   return JOURNAL_ENABLED
